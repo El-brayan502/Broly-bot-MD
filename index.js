574078c68831bcfe4aa46c19c5890e14a922134a
Eliasar YT <eliasarmoncada4@gmail.com>
2024-08-25 17:51:49 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Senna FG98', {
+say('BrolyBot-MD', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`senna-bot By FG98 Ig: @fg98_ff`, {
+say(`BrolyBot-MD El-brayan502 x Eliasar YT`, {
   font: 'console',
   align: 'center',
   gradient: ['cyan', 'magenta']
