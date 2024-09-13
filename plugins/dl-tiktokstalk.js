9d9a1ee81fb90f703fa239d3a56bc41bcdeaa0a0
Brayana-Insano👻 <brayanphonkponk@gmail.com>
2024-09-13 00:54:05 -0600

    Actualizar dl-tiktokstalk.js

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -1,27 +1,37 @@
+import axios from 'axios'
+import Starlights from '@StarlightsTeam/Scraper'
 
-import fg from 'api-dylux'
-let handler = async (m, { conn, text, args }) => {
-	
-  if (!text) throw `✳️ ${mssg.noUsername}`
-  
-  try {  	
-  let res = await fg.ttStalk(args[0])
-  let txt = `
-┌──「 *TIKTOK STALK* 
-▢ *🔖${mssg.name}:* ${res.name}
-▢ *🔖${mssg.username}:* ${res.username}
-▢ *👥${msag.followers}:* ${res.followers}
-▢ *🫂${msag.follows}:* ${res.following}
-▢ *📌${mssg.desc}:* ${res.desc}
-▢ *🔗${mssg.link}:* https://tiktok.com/${res.username}
-└────────────`
-  await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
-} catch {
-  m.reply(`✳️ ${mssg.error}`)
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
+
+  await m.react('🕓')
+  let img = await (await axios.get('https://i.ibb.co/kyTcqt9/file.jpg', { responseType: 'arraybuffer' })).data
+
+  try {
+    let data = await Starlights.tiktokSearch(text)
+
+    if (data && data.length > 0) {
+      let txt = `*乂  T I K T O K  -  S E A R C H*`
+      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
+        let video = data[i]
+        txt += `\n\n`
+        txt += `  *» Nro* : ${i + 1}\n`
+        txt += `  *» Título* : ${video.title}\n`
+        txt += `  *» Autor* : ${video.author}\n`
+        txt += `  *» Url* : ${video.url}`
+      }
+      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
+      await m.react('✅')
+    } else {
+      await conn.react('✖️')
+    }
+  } catch {
+    await m.react('✖️')
+  }
 }
-}
-handler.help = ['tiktokstalk']
-handler.tags = ['dl']
-handler.command = /^t(tstalk|iktokstalk)$/i
+handler.tags = ['search']
+handler.help = ['tiktoksearch *<búsqueda>*']
+handler.command = ['tiktoksearch', 'tiktoks']
+handler.register = true
 
 export default handler
