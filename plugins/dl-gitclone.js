9a054d097a7a4e13fb017baf3bd71918b0900290
Brayana-Insano👻 <brayanphonkponk@gmail.com>
2024-09-13 00:18:49 -0600

    Actualizar dl-gitclone.js

--- plugins/dl-gitclone.js
+++ plugins/dl-gitclone.js
@@ -2,7 +2,7 @@
 import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `${mssg.noLink('GitHub')}\n\n📌 ${mssg.example} : ${usedPrefix + command} https://github.com/FG98F/senna-bot`
+    if (!args[0]) throw `${mssg.noLink('GitHub')}\n\n📌 ${mssg.example} : ${usedPrefix + command} https://github.com/El-brayan502/Broly-bot-MD`
     if (!regex.test(args[0])) throw `⚠️ ${mssg.noLink('GitHub')}`
     let [_, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
