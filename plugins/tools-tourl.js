e95aa3e30e74624ae9d3f04f3f9d950fe94553c7
Brayana-Insano👻 <brayanphonkponk@gmail.com>
2024-09-17 08:02:09 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,21 +1,15 @@
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-
-let handler = async (m) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw `✳️ ${mssg.replyImg}`
-  let media = await q.download()
-  let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
-  let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`▢ ${media.length} Byte(s) 
-
-▢ ${isTele ? '(Sin fecha de caducidad)' : '(Desconocido)'} 
-▢ *URL :* ${link}
-  `)
-}
-handler.help = ['tourl']
-handler.tags = ['tools']
-handler.command = ['upload', 'tourl']
-
-export default handler
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+const handler = async (m) => {
+  const q = m.quoted ? m.quoted : m;
+  const mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '*🌵 Rᴇsᴘᴏɴᴅᴀ A Uɴᴀ Iᴍᴀɢᴇɴ O Uɴ Vɪᴅᴇᴏ Eʟ Cᴜᴀʟ Sᴇʀᴀ́ Cᴏɴᴠᴇʀᴛɪᴅᴏ Eɴ Eɴʟᴀᴄᴇ✨*';
+  const media = await q.download();
+  const isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+  const link = await (isTele ? uploadImage : uploadFile)(media);
+  m.reply(`*𝙴𝙽𝙻𝙰𝙲𝙴 𝙰 𝚂𝚄 𝙰𝚁𝙲𝙷𝙸𝚅𝙾:* ${link}`);
+};
+handler.help = ['tourl <reply image>'];
+handler.tags = ['sticker'];
+handler.command = /^(upload|tourl)$/i;
+export default handler;
