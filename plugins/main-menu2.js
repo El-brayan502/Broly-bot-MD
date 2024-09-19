d88b07d7f9992a6372be6ed4c4c0d9182cbe15d8
Brayana-Insano👻 <brayanphonkponk@gmail.com>
2024-09-18 20:52:11 -0600

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,30 +1,37 @@
-
 let handler = async function (m, { conn, text, usedPrefix }) {
-  
+
 let m2 = `
-≡ Use estos comandos sin el prefijo: *${usedPrefix}*.    
-╭╼⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪ 🄰🅄🄳🄸🄾🅂 ໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪
-┃֪࣪   ╰─◉⧫┈⎯🌀❈⧫⎯◉─╯
-├ׁ̟̇❍✎ Bot
-├ׁ̟̇❍✎ Buenos días
-├ׁ̟̇❍✎ Buenas tardes 
-├ׁ̟̇❍✎ Buenas noches
-├ׁ̟̇❍✎ Fino señores
-├ׁ̟̇❍✎ Sad
-╚═⎯⧫❈⠄⭎⧫❈⠄⭎⧫❈⠄⭎⧫❈⎯═╝
+≡ Use estos comandos sin el prefijo: *${usedPrefix}*
+╭━━━〔 ADUIOS 〕━━━◉
+┃╭━━━━━━━━━━━━━━━━━ 
+┃┃ Bot
+┃┃ Buenos días
+┃┃ Buenas tardes 
+┃┃ Buenas noches
+┃┃ Fino señores
+┃┃ Sad
+┃┃ Motivación
+┃┃ fiesta viernes
+┃┃ Uwu
+┃┃ Estoy triste
+┃┃ un Pato
+┃┃ ara ara
+┃┃ Bueno master
+┃╰──────────────
+╰━━━━━━━━━━━━━━◉
 `
-m.react('💽')
-    let pp = 'https://qu.ax/KQSr.jpg' 
+m.react('🍂')
+    let pp = 'https://qu.ax/cBzx.jpg' 
     /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
       ['⌬ Grupos', `${usedPrefix}gpdylux`]
     ],m, rpyt)*/
     conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rpl)
-   
+
 }
 
 handler.help = ['menu2']
 handler.tags = ['main']
 handler.command = ['menu2', 'audios'] 
 
-export default 
+export default handler
